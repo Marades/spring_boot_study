7caac388923fc4e95e37269ca66f39a22397a8cf
marades <jkl9510357@gmail.com>
2022-02-05 13:18:24 +0900

    Post api생성 및 테스트

---
+++ src/main/java/com/study/example/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.study.example.service.posts;
+
+import com.study.example.domain.posts.PostsRepository;
+import com.study.example.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}

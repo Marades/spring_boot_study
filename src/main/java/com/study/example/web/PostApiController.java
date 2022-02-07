e316f30375525e714ab22f6d38515b2301fb6e9e
marades <jkl9510357@gmail.com>
2022-02-08 02:23:46 +0900

    Post 수정 및 조회 api 작성 및 테스트

--- src/main/java/com/study/example/web/PostApiController.java
+++ src/main/java/com/study/example/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.study.example.web;
 
 import com.study.example.service.posts.PostsService;
+import com.study.example.web.dto.PostsResponseDto;
 import com.study.example.web.dto.PostsSaveRequestDto;
+import com.study.example.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,15 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
